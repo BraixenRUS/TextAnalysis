a08c7094c368f54d65b8d3afc7b4e21de3233680
Danil Tuzikov <daniltuzikov9687@gmail.com>
2025-11-07 15:40:53 +0500

    Сделал первое задание

--- SentencesParserTask.cs
+++ SentencesParserTask.cs
@@ -1,11 +1,41 @@
-﻿namespace TextAnalysis;
+﻿using System.Collections.Generic;
+using System.Linq;
 
-static class SentencesParserTask
+namespace TextAnalysis
 {
-    public static List<List<string>> ParseSentences(string text)
+    static class SentencesParserTask
     {
-        var sentencesList = new List<List<string>>();
-        //...
-        return sentencesList;
+        public static List<List<string>> ParseSentences(string text)
+        {
+            var sentencesList = new List<List<string>>();
+            var punctuation = new char[] { '.', '!', '?', ';', ':', '(', ')' };
+            var sentences = text.ToLower().Split(punctuation);
+            foreach (var sentence in sentences)
+            {
+                var words = GetWords(sentence);
+                if (words.Count > 0)
+                    sentencesList.Add(words);
+            }
+            return sentencesList;
+        }
+
+        public static List<string> GetWords(string sentence)
+        {
+            var words = new List<string>();
+            var currentWord = new List<char>();
+            foreach (var symbol in sentence)
+            {
+                if (char.IsLetter(symbol) || symbol == '\'')
+                {
+                    currentWord.Add(symbol);
+                }
+                else if (currentWord.Count > 0)
+                {
+                    words.Add(new string(currentWord.ToArray()));
+                    currentWord.Clear();
+                }
+            }
+            return words;
+        }
     }
 }
